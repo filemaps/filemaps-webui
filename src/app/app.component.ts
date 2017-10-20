c2bfdcd18e2b73d6370d7ccc0b0d18af16a4e269
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2017-10-21 01:16:40 +0300

    Better SideNav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,7 @@ declare var $: JQueryStatic;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
   title: string;
