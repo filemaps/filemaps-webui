967990c7b7ada21e94379981b3a5ad7fddf91da3
Tuomas Jaakola <tuomas.jaakola@iki.fi>
2017-12-31 22:28:57 +0200

    Year changed

--- src/app/models/resource.ts
+++ src/app/models/resource.ts
@@ -1,4 +1,4 @@
-// Copyright (c) 2017, CodeBoy. All rights reserved.
+// Copyright (c) 2018, CodeBoy. All rights reserved.
 //
 // This Source Code Form is subject to the terms of the
 // license that can be found in the LICENSE file.
